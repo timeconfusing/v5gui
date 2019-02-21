2c41dcd508cbd02d5b47586a590222d86a131ef3
timeconfusing <47795198+timeconfusing@users.noreply.github.com>
2019-02-20 21:09:09 -0700

    adds another button example

--- src/gui.cpp
+++ src/gui.cpp
@@ -4,7 +4,9 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+//#define USE_BTMN_GUI
 
+#ifdef USE_BTNM_GUI
 static lv_obj_t *g_btn_region; //tab view region of the screen
 static lv_obj_t *g_sb_region; //status bar region of the screen
 static lv_obj_t *g_sb_label;  // sb text label
@@ -82,54 +84,55 @@ void gui(void) {
   lv_btnm_set_action(btnm, btnm_action);
 }
 
-//static lv_res_t btn_click_action(lv_obj_t * btn)
-//{
-//    uint8_t id = lv_obj_get_free_num(btn);
-//
-//    printf("Button %d is released\n", id);
-//
-//    /* The button is released.
-//     * Make something here */
-//
-//    return LV_RES_OK; /*Return OK if the button is not deleted*/
-//}
-//
-//
-//void gui(void) {
-//
-//  /*Create a title label*/
-//  lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
-//  lv_label_set_text(label, "Default buttons");
-//  lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 5);
-//
-//  /*Create a normal button*/
-//  lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
-//  lv_cont_set_fit(btn1, true, true); /*Enable resizing horizontally and vertically*/
-//  lv_obj_align(btn1, label, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-//  lv_obj_set_free_num(btn1, 1);   /*Set a unique number for the button*/
-//  lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, btn_click_action);
-//
-//  /*Add a label to the button*/
-//  label = lv_label_create(btn1, NULL);
-//  lv_label_set_text(label, "Normal");
-//
-//  /*Copy the button and set toggled state. (The release action is copied too)*/
-//  lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), btn1);
-//  lv_obj_align(btn2, btn1, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-//  lv_btn_set_state(btn2, LV_BTN_STATE_TGL_REL);  /*Set toggled state*/
-//  lv_obj_set_free_num(btn2, 2);               /*Set a unique number for the button*/
-//
-//  /*Add a label to the toggled button*/
-//  label = lv_label_create(btn2, NULL);
-//  lv_label_set_text(label, "Toggled");
-//
-//  /*Copy the button and set inactive state.*/
-//  lv_obj_t * btn3 = lv_btn_create(lv_scr_act(), btn1);
-//  lv_obj_align(btn3, btn2, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
-//  lv_btn_set_state(btn3, LV_BTN_STATE_INA);   /*Set inactive state*/
-//  lv_obj_set_free_num(btn3, 3);               /*Set a unique number for the button*/
-//
-//  /*Add a label to the inactive button*/
-//  label = lv_label_create(btn3, NULL);
-//  lv_label_set_text(label, "Inactive");
-//}
+#else
+static lv_res_t btn_click_action(lv_obj_t * btn)
+{
+   uint8_t id = lv_obj_get_free_num(btn);
+   static lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+   static char buffer[32];
+
+   snprintf(buffer, 32, "Selection is %d \n", id);
+   lv_label_set_text(label, buffer);
+   lv_obj_align(label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 5);
+
+   return LV_RES_OK; /*Return OK if the button is not deleted*/
+}
+
+void gui(void) {
+
+ /*Create a title label*/
+ lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+ lv_label_set_text(label, "Selection Buttons");
+ lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 5);
+
+ /*Create a normal button*/
+ lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
+ lv_obj_align(btn1, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+ lv_obj_set_free_num(btn1, 1);   /*Set a unique number for the button*/
+ lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, btn_click_action);
+
+ /*Add a label to the button*/
+ label = lv_label_create(btn1, NULL);
+ lv_label_set_text(label, "Sel 1");
+
+ /*Copy the button and set toggled state. (The release action is copied too)*/
+ lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+ lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 0);
+ lv_obj_set_free_num(btn2, 2);               /*Set a unique number for the button*/
+ lv_btn_set_action(btn2, LV_BTN_ACTION_CLICK, btn_click_action);
+
+ /*Add a label to the toggled button*/
+ label = lv_label_create(btn2, NULL);
+ lv_label_set_text(label, "Sel 2");
+
+ /*Copy the button and set inactive state.*/
+ lv_obj_t * btn3 = lv_btn_create(lv_scr_act(), NULL);
+ lv_obj_align(btn3, NULL, LV_ALIGN_IN_RIGHT_MID, 0, 0);
+ lv_obj_set_free_num(btn3, 3);                  /*Set a unique number for the button*/
+ lv_btn_set_action(btn3, LV_BTN_ACTION_CLICK, btn_click_action);
+
+ /*Add a label to the inactive button*/
+ label = lv_label_create(btn3, NULL);
+ lv_label_set_text(label, "Sel 3");
+}
+#endif
