41469dccd03e554bd70c635771ba6c48b70338a0
timeconfusing <47795198+timeconfusing@users.noreply.github.com>
2019-03-22 22:09:45 -0700

    adds selection screen to choose which button gui to demo

--- src/gui.cpp
+++ src/gui.cpp
@@ -4,9 +4,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-//#define USE_BTNM_GUI
-
-#ifdef USE_BTNM_GUI
 static lv_obj_t *g_btn_region; //tab view region of the screen
 static lv_obj_t *g_sb_region; //status bar region of the screen
 static lv_obj_t *g_sb_label;  // sb text label
@@ -15,9 +12,9 @@ int auton_sel = 0;
 
 static lv_res_t btnm_action(lv_obj_t * btnm, const char *txt) {
 
-  int btn_num = atoi(txt);
+  int btnm_num = atoi(txt);
 
-  switch (btn_num) {
+  switch (btnm_num) {
   case 1:
     lv_label_set_text(g_sb_label, "Red Right Auton");
     auton_sel = 1;
@@ -49,28 +46,7 @@ break;
   return LV_RES_OK; /*Return OK because the button matrix is not deleted*/
 }
 
-
-void gui(void) {
-  g_btn_region = lv_obj_create(lv_scr_act(), NULL);
-  lv_obj_set_size(g_btn_region, lv_obj_get_width(lv_scr_act()),
-      lv_obj_get_height(lv_scr_act()) * 0.8);
-  lv_obj_align(g_btn_region, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
-  lv_obj_set_style(g_btn_region, &lv_style_plain);
-
-  //
-  g_sb_region = lv_obj_create(lv_scr_act(), NULL);
-  lv_obj_set_size(g_sb_region, lv_obj_get_width(lv_scr_act()),
-      lv_obj_get_height(lv_scr_act()) * 0.2);
-  //set_sb_style(g_sb_region);
-  lv_obj_align(g_sb_region, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
-  lv_obj_set_style(g_sb_region, &lv_style_plain);
-
-
-  g_sb_label = lv_label_create(g_sb_region, NULL);
-  lv_obj_set_style(g_sb_label, &lv_style_plain);
-  lv_obj_align(g_sb_label, NULL, LV_ALIGN_CENTER, 0, 0);
-
-
+void gui_btnm(void) {
   // Create a button descriptor string array w/ no repeat "\224"
   static const char * btnm_map[] = { "\2241", "\2242", "\2243", "\n",
                                      "\2244", "\2245", "\2246", "" };
@@ -84,30 +60,28 @@ void gui(void) {
   lv_btnm_set_action(btnm, btnm_action);
 }
 
-#else
-static lv_res_t btn_click_action(lv_obj_t * btn)
-{
+
+static lv_res_t btn_click_action(lv_obj_t * btn) {
    uint8_t id = lv_obj_get_free_num(btn);
-   static lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
    static char buffer[32];
+   auton_sel = id;
 
    snprintf(buffer, 32, "Selection is %d \n", id);
-   lv_label_set_text(label, buffer);
-   lv_obj_align(label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 5);
+   lv_label_set_text(g_sb_label, buffer);
 
    return LV_RES_OK; /*Return OK if the button is not deleted*/
 }
 
-void gui(void) {
+void gui_3btn(void) {
 
  /*Create a title label*/
- lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+ lv_obj_t * label = lv_label_create(g_btn_region, NULL);
  lv_label_set_text(label, "Selection Buttons");
  lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 5);
 
  /*Create a normal button*/
- lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
- lv_obj_align(btn1, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+ lv_obj_t * btn1 = lv_btn_create(g_btn_region, NULL);
+ lv_obj_align(btn1, NULL, LV_ALIGN_IN_LEFT_MID, 50, 0);
  lv_obj_set_free_num(btn1, 1);   /*Set a unique number for the button*/
  lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, btn_click_action);
 
@@ -116,7 +90,7 @@ void gui(void) {
  lv_label_set_text(label, "Sel 1");
 
  /*Copy the button and set toggled state. (The release action is copied too)*/
- lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+ lv_obj_t * btn2 = lv_btn_create(g_btn_region, NULL);
  lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 0);
  lv_obj_set_free_num(btn2, 2);               /*Set a unique number for the button*/
  lv_btn_set_action(btn2, LV_BTN_ACTION_CLICK, btn_click_action);
@@ -126,8 +100,8 @@ void gui(void) {
  lv_label_set_text(label, "Sel 2");
 
  /*Copy the button and set inactive state.*/
- lv_obj_t * btn3 = lv_btn_create(lv_scr_act(), NULL);
- lv_obj_align(btn3, NULL, LV_ALIGN_IN_RIGHT_MID, 0, 0);
+ lv_obj_t * btn3 = lv_btn_create(g_btn_region, NULL);
+ lv_obj_align(btn3, NULL, LV_ALIGN_IN_RIGHT_MID, -50, 0);
  lv_obj_set_free_num(btn3, 3);                  /*Set a unique number for the button*/
  lv_btn_set_action(btn3, LV_BTN_ACTION_CLICK, btn_click_action);
 
@@ -135,4 +109,65 @@ void gui(void) {
  label = lv_label_create(btn3, NULL);
  lv_label_set_text(label, "Sel 3");
 }
-#endif
+
+uint8_t demo_id = 0;
+
+static lv_res_t demo_click_action(lv_obj_t * btn) {
+   demo_id = lv_obj_get_free_num(btn);
+
+   g_btn_region = lv_obj_create(lv_scr_act(), NULL);
+   lv_obj_set_size(g_btn_region, lv_obj_get_width(lv_scr_act()),
+       lv_obj_get_height(lv_scr_act()) * 0.8);
+   lv_obj_align(g_btn_region, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+   lv_obj_set_style(g_btn_region, &lv_style_pretty_color);
+
+   //
+   g_sb_region = lv_obj_create(lv_scr_act(), NULL);
+   lv_obj_set_size(g_sb_region, lv_obj_get_width(lv_scr_act()),
+       lv_obj_get_height(lv_scr_act()) * 0.2);
+   lv_obj_align(g_sb_region, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+   lv_obj_set_style(g_sb_region, &lv_style_pretty_color);
+
+   g_sb_label = lv_label_create(g_sb_region, NULL);
+   lv_obj_set_style(g_sb_label, &lv_style_pretty_color);
+   lv_obj_align(g_sb_label, NULL, LV_ALIGN_CENTER, 0, 0);
+
+   if (demo_id==1) {
+    gui_btnm();
+   } else {
+    gui_3btn();
+   }
+
+   return LV_RES_OK; /*Return OK if the button is not deleted*/
+}
+
+
+void gui(void) {
+
+// select demo
+  /*Create a title label*/
+  lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+  lv_label_set_text(label, "Selection A Demo");
+  lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_MID, 0, 5);
+
+  /*Create a normal button*/
+  lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, -150, 0);
+  lv_obj_set_free_num(btn1, 1);   /*Set a unique number for the button*/
+  lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, demo_click_action);
+
+  /*Add a label to the button*/
+  label = lv_label_create(btn1, NULL);
+  lv_label_set_text(label, "MATRIX");
+
+  /*Copy the button and set toggled state. (The release action is copied too)*/
+  lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 150, 0);
+  lv_obj_set_free_num(btn2, 2);               /*Set a unique number for the button*/
+  lv_btn_set_action(btn2, LV_BTN_ACTION_CLICK, demo_click_action);
+
+  /*Add a label to the toggled button*/
+  label = lv_label_create(btn2, NULL);
+  lv_label_set_text(label, "BUTTONS");
+
+}
